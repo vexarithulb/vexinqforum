e75b54ff3f6a54aee6598b0e7cb59c0052446b2e
vexarithulb <mustfafx436@gmail.com>
2025-06-30 10:55:48 +0300

    Create forum.sql

---
+++ forum.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE forum;
+
+USE forum;
+
+CREATE TABLE threads (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
